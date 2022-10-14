3b2ffccc885e8018a4ed010fbfd3af20645cecd5
Sami <sandramilenamartinezarrieta@hotmail.com>
2022-10-14 18:47:13 +0000

    diagramUML

--- src/models.py
+++ src/models.py
@@ -8,26 +8,79 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+# class Person(Base):
+#     __tablename__ = 'person'
+#     # Here we define columns for the table person
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250), nullable=False)
+    
+
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
+
+#     def to_dict(self):
+#         return {}
+
+
+
+class Usuario(Base):
+    __tablename__ = 'usuario'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+class Planeta(Base):
+    __tablename__ = 'planeta'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    rotation_period = Column(Integer, nullable=True)
+    orbital_period = Column(Integer, nullable=True)
+    diameter = Column(Integer, nullable=True)
+    gravity = Column(Integer, nullable=True)
+
+
+class Personaje (Base):
+    __tablename__ = 'personaje'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    hair_color = Column(String(250), nullable=True)
+    eye_color = Column(String(250), nullable=True)
+    birth_year = Column(Integer, nullable=True)
+    gender = Column(String(20), nullable=True)
+
+class Vehiculo (Base):
+    __tablename__ = 'vehiculo'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+
+class Favoritos (Base):
+    __tablename__ = 'favorito'
+    # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
-    def to_dict(self):
-        return {}
+    id_usuario = Column(Integer,ForeignKey('usuario.id'),nullable=False)
+    id_planeta = Column(Integer,ForeignKey('planeta.id'),nullable=True)
+    id_personaje = Column(Integer,ForeignKey('personaje.id'),nullable=True)
+    id_vehiculo = Column(Integer,ForeignKey('vehiculo.id'),nullable=True)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
